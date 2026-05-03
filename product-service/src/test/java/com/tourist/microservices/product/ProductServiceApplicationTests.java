48b8bb46167d83604eedda2c562667a0ce4e6496
The-Tourist-170 <ayushya.official@gmail.com>
2026-05-03 17:10:57 +0530

    Final Product Service

--- product-service/src/test/java/com/tourist/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/tourist/microservices/product/ProductServiceApplicationTests.java
@@ -2,33 +2,31 @@ package com.tourist.microservices.product;
 
 import com.tourist.microservices.product.model.Product;
 import com.tourist.microservices.product.repository.ProductRepository;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import java.math.BigDecimal;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Autowired
-	private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {}
 
-	@Test
-	void shouldCreateProduct() {
-		Product product = Product.builder()
-				.name("iPhone 15")
-				.description("iPhone 15")
-				.price(BigDecimal.valueOf(1000))
-				.build();
-		productRepository.save(product);
-		Assertions.assertNotNull(product.getId());
-	}
-
+    @Test
+    void shouldCreateProduct() {
+        Product product = Product.builder()
+            .name("iPhone 15")
+            .description("iPhone 15")
+            .price(BigDecimal.valueOf(1000))
+            .build();
+        productRepository.save(product);
+        Assertions.assertNotNull(product.getId());
+    }
 }
